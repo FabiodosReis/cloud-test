1bc6c50e7ad2c3530eae46629ce87b9653b92696
Fabio Reis <fabioreis@MacBook-Air-de-Fabio.local>
2025-04-06 10:11:01 -0300

    initial commit

---
+++ src/main/kotlin/com/cloud/project/for/test/dployment/ProjectForTestDploymentApplication.kt
@@ -0,0 +1,11 @@
+package com.cloud.project.`for`.test.dployment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectForTestDploymentApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjectForTestDploymentApplication>(*args)
+}
---
+++ src/test/kotlin/com/cloud/project/for/test/dployment/ProjectForTestDploymentApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cloud.project.`for`.test.dployment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectForTestDploymentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
